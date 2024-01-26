b03a8cdcd4b0f604eeef52dadb6e959181d4f1e2
alessandrogelmi <alessandro.gelmi@cinetix-team.it>
2024-01-26 17:02:51 +0100

    feat: status API

--- src/main/java/it/pagopa/pn/bff/rest/StatusController.java
+++ src/main/java/it/pagopa/pn/bff/rest/StatusController.java
@@ -1,19 +1,16 @@
 package it.pagopa.pn.bff.rest;
 
-import io.swagger.annotations.ApiOperation;
 import it.pagopa.pn.bff.generated.openapi.server.v1.api.StatusTemplateApi;
-import it.pagopa.pn.bff.generated.openapi.server.v1.api.TemplateSampleApi;
 import it.pagopa.pn.bff.service.StatusService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @Slf4j
 @RestController
-public class StatusController implements StatusTemplateApi {
+public class StatusController implements StatusTemplateApi  {
 
     private final StatusService statusService;
 
@@ -21,7 +18,9 @@ public class StatusController implements StatusTemplateApi {
         this.statusService = statusService;
     }
 
-    public Mono<ResponseEntity<String>> getStatus() {
-        return statusService.getStatus().map(ResponseEntity::ok);
+    @Override
+    public Mono<ResponseEntity<String>> getStatus(final ServerWebExchange exchange) {
+        return statusService.getStatus()
+                .map(status -> ResponseEntity.ok().body(status));
     }
 }
