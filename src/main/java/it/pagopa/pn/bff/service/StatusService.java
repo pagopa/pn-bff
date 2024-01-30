eed6e38d6643d8bb00b228d88da662120ca20bee
Alessandro Gelmi <alessandro.gelmi@cinetix-team.it>
2024-01-30 11:17:44 +0100

    feat(pn-5211) -  Add status path to openapi config file (#9)

--- src/main/java/it/pagopa/pn/bff/service/StatusService.java
+++ src/main/java/it/pagopa/pn/bff/service/StatusService.java
@@ -1,13 +1,15 @@
 package it.pagopa.pn.bff.service;
 
+import it.pagopa.pn.bff.generated.openapi.server.v1.dto.StatusSchema;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 @Slf4j
 @Service
 public class StatusService {
-    public Mono<String> getStatus() {
-        return Mono.just("I'm alive!");
+    public Mono<ResponseEntity<StatusSchema>> getStatus() {
+        return Mono.just(ResponseEntity.ok(StatusSchema.builder().status("OK").build()));
     }
 }
