b8a4075a742fd3c26913c049fe1a2d417fb74882
Giacomo Vallorani <giacomo.vallorani@pagopa.it>
2024-05-07 11:06:11 +0200

    feat(PN-10901): add sts implementation

---
+++ functions/senderDashboardDataIndexer/src/app/sts.js
@@ -0,0 +1,26 @@
+import { STSClient, AssumeRoleCommand } from '@aws-sdk/client-sts';
+
+/**
+ * Gets temporary security credentials by assuming IAM role.
+ *
+ * @return {Promise<Object>} Temporary security credentials: Access Key ID,
+ * Secret Access Key and Session Token
+ */
+export const getAssumeRoleCredentials = async (
+  roleArn,
+  roleSessionName,
+  region = 'eu-south-1'
+) => {
+  const stsClient = new STSClient({ region: region });
+  const assumeRoleCommand = new AssumeRoleCommand({
+    RoleArn: roleArn,
+    RoleSessionName: roleSessionName,
+  });
+
+  const { Credentials } = await stsClient.send(assumeRoleCommand);
+  return {
+    accessKeyId: Credentials.AccessKeyId,
+    secretAccessKey: Credentials.SecretAccessKey,
+    sessionToken: Credentials.SessionToken,
+  };
+};
