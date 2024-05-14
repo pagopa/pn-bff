9e3c02dccb93ff7d9fe8b4455faec849571bcb9e
Giacomo Vallorani <giacomo.vallorani@pagopa.it>
2024-05-14 10:15:58 +0200

    feat(PN-10901): add streamLine test

--- functions/senderDashboardDataIndexer/src/test/s3.test.js
+++ functions/senderDashboardDataIndexer/src/test/s3.test.js
@@ -4,6 +4,7 @@ const chaiAsPromised = require('chai-as-promised');
 const { sdkStreamMixin } = require('@smithy/util-stream');
 const { mockClient } = require('aws-sdk-client-mock');
 const fs = require('node:fs');
+const { PassThrough } = require('stream');
 const {
   S3Client,
   GetObjectCommand,
@@ -46,19 +47,57 @@ describe('s3 tests', function () {
   });
 
   it('should handle stream error in streamLines', async () => {
+    // Given
     const s3Mock = mockClient(S3Client);
     const errorStream = new require('stream').Readable();
+    s3Mock.on(GetObjectCommand).resolves({ Body: errorStream });
+    const s3 = new S3Client({});
+
+    // When
     errorStream._read = () => {
       errorStream.emit('error', new Error('Stream error'));
     };
 
-    s3Mock.on(GetObjectCommand).resolves({ Body: errorStream });
-
-    const s3 = new S3Client({});
+    // Then
     await expect(
       streamLines(s3, 'test', 'test', () => {})
     ).to.eventually.be.rejectedWith('Stream error');
     s3Mock.reset();
+  });
+
+  describe('streamLines', () => {
+    it('correctly processes lines split across chunks', async () => {
+      // Given
+      const stream = new PassThrough();
+      const results = [];
+
+      // When
+      const processLines = streamLines(
+        { send: () => ({ Body: stream }) },
+        'bucket',
+        'key',
+        (line, start, end) => {
+          results.push(line);
+        }
+      );
+
+      stream.write('Hello, Wo');
+      stream.write('rld!\nThis');
+      stream.write(' is a test.\nAnother line');
+      stream.end();
+
+      // Wait for the promise from streamLines to resolve
+      const { lines, bytes } = await processLines;
+
+      // Then
+      expect(results).to.deep.equal([
+        'Hello, World!',
+        'This is a test.',
+        'Another line',
+      ]);
+      expect(lines).to.equal(3);
+      expect(bytes).to.be.greaterThan(0);
+    });
   });
 
   it('should writeObject', async () => {
