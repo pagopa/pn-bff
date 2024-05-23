a70bb7be36c839196e6abb29d850895802178988
Giacomo Vallorani <giacomo.vallorani@pagopa.it>
2024-05-23 16:46:00 +0200

    refactor(PN-10908): add error handling

--- src/main/java/it/pagopa/pn/bff/rest/SenderDashboardController.java
+++ src/main/java/it/pagopa/pn/bff/rest/SenderDashboardController.java
@@ -26,13 +26,13 @@ public class SenderDashboardController implements SenderDashboardApi {
      * GET /bff/v1/sender-dashboard/dashboard-data-request/{cxType}/{cxId}: data request
      * Get dashboard data
      *
-     * @param xPagopaPnCxType The type of the user
-     * @param xPagopaPnCxId   The id of the user
-     * @param cxType          The type of the user
-     * @param cxId            The id of the user
+     * @param xPagopaPnCxType Public Administration Type
+     * @param xPagopaPnCxId   Public Administration id
+     * @param cxType          Public Administration Type
+     * @param cxId            Public Administration id
      * @param startDate       The search start date
      * @param endDate         The search end date (included)
-     * @return the detailed list of payments
+     * @return BffSenderDashboardDataResponse
      */
     @Override
     public Mono<ResponseEntity<BffSenderDashboardDataResponse>> getDashboardDataV1(
@@ -44,11 +44,14 @@ public class SenderDashboardController implements SenderDashboardApi {
             LocalDate endDate,
             final ServerWebExchange exchange) {
         log.logStartingProcess("getDashboardDataV1");
-//        if (! xPagopaPnCxType.getValue().equals(cxType) || ! xPagopaPnCxId.equals(cxId)) {
-//            return null; // TODO 400 invalid input
-//        }
         Mono<BffSenderDashboardDataResponse> serviceResponse =
-                senderDashboardService.getDashboardData(xPagopaPnCxType.getValue(), xPagopaPnCxId, startDate, endDate);
+                senderDashboardService.getDashboardData(
+                        xPagopaPnCxType.getValue(),
+                        xPagopaPnCxId,
+                        cxType,
+                        cxId,
+                        startDate,
+                        endDate);
         log.logEndingProcess("getDashboardDataV1");
         return serviceResponse.map(response -> ResponseEntity.status(HttpStatus.OK).body(response));
     }
--- src/main/java/it/pagopa/pn/bff/service/senderdashboard/SenderDashboardService.java
+++ src/main/java/it/pagopa/pn/bff/service/senderdashboard/SenderDashboardService.java
@@ -4,6 +4,7 @@ import it.pagopa.pn.bff.exceptions.PnBffException;
 import it.pagopa.pn.bff.exceptions.PnBffExceptionCodes;
 import it.pagopa.pn.bff.generated.openapi.server.v1.dto.BffSenderDashboardDataResponse;
 import it.pagopa.pn.bff.service.senderdashboard.connectors.DatalakeS3Resource;
+import it.pagopa.pn.bff.service.senderdashboard.exceptions.SenderNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Slf4j
 @Service
@@ -19,11 +21,46 @@ public class SenderDashboardService {
 
     private final DatalakeS3Resource datalakeResource;
 
-    public Mono<BffSenderDashboardDataResponse> getDashboardData(String cxType, String cxId, LocalDate startDate,
+
+    /**
+     * Fetches dashboard data for the specified sender.
+     *
+     * @param cxType    Public Administration Type
+     * @param cxId      Public Administration id
+     * @param pathCxType Public Administration Type
+     * @param pathCxId  Public Administration id
+     * @param startDate the start date for the data request
+     * @param endDate   the end date for the data request.
+     * @return a {@link Mono} emitting the {@link BffSenderDashboardDataResponse}.
+     */
+    public Mono<BffSenderDashboardDataResponse> getDashboardData(
+            String cxType,
+            String cxId,
+            String pathCxType,
+            String pathCxId,
+            LocalDate startDate,
             LocalDate endDate) {
 
-        // TODO checkCxType
+        if(!Objects.equals(cxType, pathCxType) || !Objects.equals(cxId, pathCxId)) {
+            return Mono.error(
+                    new PnBffException(
+                        "cxType and cxId mismatch",
+                        "The provided cxType and cxId do not match the values in the headers x-pagopa-pn-cx-type and x-pagopa-pn-cx-id",
+                        HttpStatus.BAD_REQUEST.value(),
+                        PnBffExceptionCodes.ERROR_CODE_PN_GENERIC_ERROR
+                    ));
+        }
+
         return Mono.fromCallable(() -> datalakeResource.getDataResponse(cxId, startDate, endDate))
+                .onErrorMap(SenderNotFoundException.class, e -> {
+                    log.error("Exception occurred while fetching data from Datalake", e);
+                    return new PnBffException(
+                            "cxId not found ",
+                            "cxId not found ",
+                            HttpStatus.NOT_FOUND.value(),
+                            PnBffExceptionCodes.ERROR_CODE_PN_GENERIC_ERROR
+                    );
+                })
                 .onErrorMap(Exception.class,  e -> {
                     log.error("Exception occurred while fetching data from Datalake", e);
                     return new PnBffException(
