e5dbefe5b3bf43b78e073233de20f7635ac5b75c
Andrea Cimini <andrea.cimini@pagopa.it>
2024-05-23 12:09:33 +0200

    fixed error code for legal fact category not found

--- src/main/java/it/pagopa/pn/bff/exceptions/PnBffExceptionCodes.java
+++ src/main/java/it/pagopa/pn/bff/exceptions/PnBffExceptionCodes.java
@@ -5,6 +5,6 @@ import it.pagopa.pn.commons.exceptions.PnExceptionsCodes;
 public class PnBffExceptionCodes extends PnExceptionsCodes {
     public static final String ERROR_CODE_BFF_BODYNOTFOUND = "PN_BFF_BODYNOTFOUND";
     public static final String ERROR_CODE_BFF_DOCUMENTIDNOTFOUND = "PN_BFF_DOCUMENTIDNOTFOUND";
-    public static final String ERROR_CODE_BFF_LEGALFACTCATEGORYNOTFOUND = "ERROR_CODE_BFF_LEGALFACTCATEGORYNOTFOUND";
+    public static final String ERROR_CODE_BFF_LEGALFACTCATEGORYNOTFOUND = "PN_BFF_LEGALFACTCATEGORYNOTFOUND";
     public static final String ERROR_CODE_BFF_INVALIDBODY = "PN_BFF_INVALIDBODY";
 }
