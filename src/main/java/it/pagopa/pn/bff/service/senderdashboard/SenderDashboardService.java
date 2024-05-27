0dbbedf29a735c1e473e31ee91871278422296e2
Giacomo Vallorani <giacomo.vallorani@pagopa.it>
2024-05-27 18:23:11 +0200

    style(PN-10908): remove unused import

--- src/main/java/it/pagopa/pn/bff/service/senderdashboard/SenderDashboardService.java
+++ src/main/java/it/pagopa/pn/bff/service/senderdashboard/SenderDashboardService.java
@@ -9,7 +9,6 @@ import it.pagopa.pn.bff.service.senderdashboard.exceptions.SenderNotFoundExcepti
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
