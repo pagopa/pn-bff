43e7706615ed642d674198e55b3aa128c08e124c
Andrea Cimini <andrea.cimini@pagopa.it>
2024-08-30 17:32:20 +0200

    converted inputs to string

--- .github/workflows/actions/update-dependencies/src/input-helper.js
+++ .github/workflows/actions/update-dependencies/src/input-helper.js
@@ -4,47 +4,47 @@ function getDependencies() {
     const dependencies = [];
     const authFleet = core.getInput('pn-auth-fleet');
     core.debug(`pn-auth-fleet = ${authFleet}`);
-    if (authFleet) {
+    if (authFleet === "true") {
         dependencies.push('pn-auth-fleet');
     }
     const commons = core.getInput('pn-commons');
     core.debug(`pn-commons = ${commons}`);
-    if (commons) {
+    if (commons === "true") {
         dependencies.push('pn-commons');
     }
     const delivery = core.getInput('pn-delivery');
     core.debug(`pn-delivery = ${delivery}`);
-    if (delivery) {
+    if (delivery === "true") {
         dependencies.push('pn-delivery');
     }
     const apiKeyManager = core.getInput('pn-apikey-manager');
     core.debug(`pn-apikey-manager = ${apiKeyManager}`);
-    if (apiKeyManager) {
+    if (apiKeyManager === "true") {
         dependencies.push('pn-apikey-manager');
     }
     const externalRegistries = core.getInput('pn-external-registries');
     core.debug(`pn-external-registries = ${externalRegistries}`);
-    if (externalRegistries) {
+    if (externalRegistries === "true") {
         dependencies.push('pn-external-registries');
     }
     const userAttributes = core.getInput('pn-user-attributes');
     core.debug(`pn-user-attributes = ${userAttributes}`);
-    if (userAttributes) {
+    if (userAttributes === "true") {
         dependencies.push('pn-user-attributes');
     }
     const downtimeLogs = core.getInput('pn-downtime-logs');
     core.debug(`pn-downtime-logs = ${downtimeLogs}`);
-    if (downtimeLogs) {
+    if (downtimeLogs === "true") {
         dependencies.push('pn-downtime-logs');
     }
     const deliveryPush = core.getInput('pn-delivery-push');
     core.debug(`pn-delivery-push = ${deliveryPush}`);
-    if (deliveryPush) {
+    if (deliveryPush === "true") {
         dependencies.push('pn-delivery-push');
     }
     const mandate = core.getInput('pn-mandate');
     core.debug(`pn-mandate = ${mandate}`);
-    if (mandate) {
+    if (mandate === "true") {
         dependencies.push('pn-mandate');
     }
     return dependencies;
