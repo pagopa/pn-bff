ec65733ae5a27747cc02505b5587b2bb81f9e0de
Andrea Cimini <andrea.cimini@pagopa.it>
2024-09-02 15:54:19 +0200

    created run method

--- .github/workflows/actions/update-dependencies/src/index.js
+++ .github/workflows/actions/update-dependencies/src/index.js
@@ -4,20 +4,24 @@ const github = require('@actions/github');
 const {getDependencies} = require('./input-helper');
 const {getLastTagCommitId, initOctokitClient} = require('./repository-helper');
 
-try {
-  // read dependencies to update
-  const dependencies = getDependencies();
-  if (dependencies.length > 0) {
-    core.info(`Chosen dependencies to update = ${dependencies.join(', ')}`);
-    // for those dependencies chosen get the commit id of the last tag
-    const commitIds = {};
-    initOctokitClient();
-    for (const dependency of dependencies) {
-        commitIds[dependency] = await getLastTagCommitId(octokit, dependency);
+async function run() {
+    try {
+      // read dependencies to update
+      const dependencies = getDependencies();
+      if (dependencies.length > 0) {
+        core.info(`Chosen dependencies to update = ${dependencies.join(', ')}`);
+        // for those dependencies chosen get the commit id of the last tag
+        const commitIds = {};
+        initOctokitClient();
+        for (const dependency of dependencies) {
+            commitIds[dependency] = await getLastTagCommitId(octokit, dependency);
+        }
+        return;
+      }
+      throw new Error(`No dependencies chosen`);
+    } catch (error) {
+      core.setFailed(error.message);
     }
-    return;
-  }
-  throw new Error(`No dependencies chosen`);
-} catch (error) {
-  core.setFailed(error.message);
 }
+
+run();
