41af97b3984136f22d6dcbe3bf8bf96f3c738b7d
Andrea Cimini <andrea.cimini@pagopa.it>
2024-09-03 15:39:21 +0200

    replace pom content

--- .github/workflows/actions/update-dependencies/src/file-helper.js
+++ .github/workflows/actions/update-dependencies/src/file-helper.js
@@ -6,19 +6,27 @@ const POM_PATH = './pom.xml';
 const GITHUB_ROOT_PATH = 'https://raw.githubusercontent.com';
 const GITHUB_OPENAPI_FILE_PATH = 'docs/openapi';
 
+function getGitHubOpenapiRegexp(commitIds) {
+    const dependencies = Object.keys(commitIds);
+    const regexp = new RegExp(`${GITHUB_ROOT_PATH}/${github.context.repo.owner}/(?<repository>${dependencies.join('|')})/(?<commitId>.+)/${GITHUB_OPENAPI_FILE_PATH}/(?<openapiFile>.+).yaml`, 'g');
+    core.debug(`Computed regular expression ${regexp.toString()}`);
+    return regexp;
+}
+
 function updatePom(commitIds) {
     core.debug(`Reading pom at path ${POM_PATH}`);
     try {
+        // read content
         let content = fs.readFileSync(POM_PATH, 'utf8');
         core.debug(`Updating pom`);
-        const dependencies = Object.keys(commitIds);
-        const regexp = new RegExp(`${GITHUB_ROOT_PATH}/${github.context.repo.owner}/(?<repository>${dependencies.join('|')})/(?<commitId>.+)/${GITHUB_OPENAPI_FILE_PATH}/.+.yaml`, 'g');
-        core.debug(`Computed regular expression ${regexp.toString()}`);
-        content.replace(regexp, (match, repository, commitId) => {
-            core.info(`match ${match}`);
-            core.info(`repository ${repository}`);
-            core.info(`commitId ${commitId}`);
-        })
+        // change content
+        content = content.replace(getGitHubOpenapiRegexp(commitIds), (match, repository, commitId, openapiFile) => {
+            core.debug(`match ${match}`);
+            core.debug(`repository ${repository}`);
+            core.debug(`commitId ${commitId}`);
+            return `${GITHUB_ROOT_PATH}/${github.context.repo.owner}/${repository}/${commitIds[repository]}/${GITHUB_OPENAPI_FILE_PATH}/${openapiFile}.yaml`
+        });
+        core.info(content);
     } catch (error) {
         throw new Error(`Error reading pom: ${error}`);
     }
