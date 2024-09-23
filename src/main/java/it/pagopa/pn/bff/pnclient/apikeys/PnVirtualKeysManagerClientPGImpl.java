91ae2106b961e241f71e39d52015b7f908a5f127
leleOFA <emanuele.didonato@pagopa.it>
2024-09-23 10:05:04 +0200

    add bean for virtual keys api

--- src/main/java/it/pagopa/pn/bff/pnclient/apikeys/PnVirtualKeysManagerClientPGImpl.java
+++ src/main/java/it/pagopa/pn/bff/pnclient/apikeys/PnVirtualKeysManagerClientPGImpl.java
@@ -14,6 +14,7 @@ import java.util.List;
 @CustomLog
 @RequiredArgsConstructor
 public class PnVirtualKeysManagerClientPGImpl {
+
     private final VirtualKeysApi virtualKeysApi;
 
     public Mono<VirtualKeysResponse> getVirtualKeys(String xPagopaPnUid, it.pagopa.pn.bff.generated.openapi.msclient.virtualkey_pg.model.CxTypeAuthFleet xPagopaPnCxType,
