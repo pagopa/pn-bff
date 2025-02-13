8880cc84ac5f5193baeaec4dd576279e1c6dc0c5
Francesco Bianchi <f.bianchi@codermine.com>
2025-02-13 14:33:05 +0100

    add javadoc to mapper

--- src/main/java/it/pagopa/pn/bff/mappers/payments/PaymentsTppMapper.java
+++ src/main/java/it/pagopa/pn/bff/mappers/payments/PaymentsTppMapper.java
@@ -5,9 +5,18 @@ import it.pagopa.pn.bff.generated.openapi.server.v1.dto.notifications.BffPayment
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
+/**
+ * Mapstruct mapper interface, used to map the PaymentUrlResponse to the BffPaymentTppResponse
+ */
 @Mapper
 public interface PaymentsTppMapper {
     PaymentsTppMapper modelMapper = Mappers.getMapper(PaymentsTppMapper.class);
 
+    /**
+     * Maps a PaymentUrlResponse to a BffPaymentTppResponse
+     *
+     * @param response the PaymentUrlResponse to map
+     * @return the mapped BffPaymentTppResponse
+     */
     BffPaymentTppResponse mapPaymentTppResponse(PaymentUrlResponse response);
 }
