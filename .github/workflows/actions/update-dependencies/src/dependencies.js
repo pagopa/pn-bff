6f7b984b1093d17b5cbd7c84a5260764e32b331a
Francesco Bianchi <105631409+fbianchicodermine@users.noreply.github.com>
2025-03-27 15:44:31 +0100

    feat(PN-13847): update action "update dep" (#166)

---
+++ .github/workflows/actions/update-dependencies/src/dependencies.js
@@ -0,0 +1,18 @@
+const dependencies = [
+  "pn-commons",
+  "pn-model",
+  "pn-parent",
+  "pn-auth-fleet",
+  "pn-delivery",
+  "pn-apikey-manager",
+  "pn-external-registries",
+  "pn-user-attributes",
+  "pn-downtime-logs",
+  "pn-delivery-push",
+  "pn-mandate",
+  "pn-emd-integration",
+];
+
+module.exports = {
+  dependencies,
+};
