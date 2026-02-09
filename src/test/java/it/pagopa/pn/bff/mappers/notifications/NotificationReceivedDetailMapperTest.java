aab26e9362215a6efdf6a706d5f78a6231e926f2
Sarah Donvito <sarah.donvito@pagopa.it>
2026-02-09 15:48:34 +0100

    restore test

--- src/test/java/it/pagopa/pn/bff/mappers/notifications/NotificationReceivedDetailMapperTest.java
+++ src/test/java/it/pagopa/pn/bff/mappers/notifications/NotificationReceivedDetailMapperTest.java
@@ -1,39 +1,20 @@
 package it.pagopa.pn.bff.mappers.notifications;
 
 import it.pagopa.pn.bff.generated.openapi.msclient.delivery_recipient.model.FullReceivedNotificationV27;
-import it.pagopa.pn.bff.generated.openapi.msclient.delivery_recipient.model.TimelineElementCategoryV28;
-import it.pagopa.pn.bff.generated.openapi.msclient.delivery_recipient.model.TimelineElementV28;
 import it.pagopa.pn.bff.generated.openapi.server.v1.dto.notifications.BffFullNotificationV1;
 import org.junit.jupiter.api.Test;
-import java.util.Collections;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class NotificationReceivedDetailMapperTest {
-
     @Test
-    void shouldMapNotificationWithTimelineLogic() {
+    void testReceivedNotificationDetailMapper() {
         FullReceivedNotificationV27 notification = new FullReceivedNotificationV27();
-        notification.setIun("IUN-TEST-2024");
+        BffFullNotificationV1 actualMapNotificationDetailResult = NotificationReceivedDetailMapper.modelMapper.mapReceivedNotificationDetail(notification);
+        assertNotNull(actualMapNotificationDetailResult);
 
-        TimelineElementV28 timelineElement = new TimelineElementV28();
-        timelineElement.setCategory(TimelineElementCategoryV28.REQUEST_ACCEPTED);
-        notification.setTimeline(Collections.singletonList(timelineElement));
-
-        BffFullNotificationV1 result = NotificationReceivedDetailMapper.modelMapper.mapReceivedNotificationDetail(notification);
-
-        assertNotNull(result, "The result should not be null");
-        assertEquals("IUN-TEST-2024", result.getIun());
-
-        assertFalse(result.getTimeline().isEmpty(), "Timeline should not be empty");
-        assertEquals(0, result.getTimeline().get(0).getIndex(), "The index of the first timeline element should be 0");
-        assertNotNull(result.getTimeline().get(0).getHidden(), "The hidden property should not be null");
-    }
-
-
-    @Test
-    void shouldReturnNullWhenInputIsNull() {
-        BffFullNotificationV1 result = NotificationReceivedDetailMapper.modelMapper.mapReceivedNotificationDetail(null);
-        assertNull(result);
+        BffFullNotificationV1 mapNotificationNull = NotificationReceivedDetailMapper.modelMapper.mapReceivedNotificationDetail(null);
+        assertNull(mapNotificationNull);
     }
 }
