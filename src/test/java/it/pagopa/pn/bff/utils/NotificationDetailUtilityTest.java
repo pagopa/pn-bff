6d9444e50ad176f4050702bd47b32aba7b8367b1
Sarah Donvito <sarah.donvito@pagopa.it>
2026-02-11 12:18:53 +0100

    cr

--- src/test/java/it/pagopa/pn/bff/utils/NotificationDetailUtilityTest.java
+++ src/test/java/it/pagopa/pn/bff/utils/NotificationDetailUtilityTest.java
@@ -671,12 +671,19 @@ class NotificationDetailUtilityTest {
 
     @Test
     void insertInvalidateElementsInTimeline() {
+
         BffFullNotificationV1 bffFullNotificationV1 = new BffFullNotificationV1();
+        List<BffNotificationStatusHistory> notificationStatusHistory = new ArrayList<>();
+        BffNotificationStatusHistory elementStatusHistory = new BffNotificationStatusHistory();
+        elementStatusHistory.setStatus(BffNotificationStatus.DELIVERING);
+        notificationStatusHistory.add(elementStatusHistory);
+        bffFullNotificationV1.setNotificationStatusHistory(notificationStatusHistory);
         List<BffNotificationDetailTimeline> timeline = new ArrayList<>();
         BffNotificationDetailTimeline reworkedElementTimeline = new BffNotificationDetailTimeline();
         reworkedElementTimeline.setCategory(BffTimelineCategory.NOTIFICATION_TIMELINE_REWORKED);
         reworkedElementTimeline.setTimestamp(OffsetDateTime.now());
         BffNotificationDetailTimelineDetails reworkedDetails = new BffNotificationDetailTimelineDetails();
+
         List<NotificationStatusHistoryInvalidatedElement> invalidatedElements = new ArrayList<>();
         NotificationStatusHistoryInvalidatedElement invalidatedElement = new NotificationStatusHistoryInvalidatedElement();
         List<it.pagopa.pn.bff.generated.openapi.server.v1.dto.notifications.TimelineElementV28> relatedTimelineElements = new ArrayList<>();
@@ -685,20 +692,31 @@ class NotificationDetailUtilityTest {
         relatedTimelineElement.setCategory(
                 it.pagopa.pn.bff.generated.openapi.server.v1.dto.notifications.TimelineElementCategoryV28.SEND_ANALOG_PROGRESS);
         relatedTimelineElement.setTimestamp(OffsetDateTime.now());
+
         relatedTimelineElements.add(relatedTimelineElement);
         NotificationStatusV26 status = NotificationStatusV26.DELIVERING;
         invalidatedElement.setRelatedTimelineElements(relatedTimelineElements);
         invalidatedElement.setStatus(status);
         invalidatedElement.setActiveFrom(OffsetDateTime.now());
+
         invalidatedElements.add(invalidatedElement);
+
         reworkedDetails.setInvalidatedTimelineAndStatusHistory(invalidatedElements);
+
         reworkedElementTimeline.setDetails(reworkedDetails);
         timeline.add(reworkedElementTimeline);
         bffFullNotificationV1.setTimeline(timeline);
+
          NotificationDetailUtility.insertInvalidateElementsInTimeline(bffFullNotificationV1);
             assertTrue(bffFullNotificationV1.getTimeline().stream()
                     .anyMatch(element -> element.getElementId() != null && element.getElementId().equals("relatedElementId"))
             );
+        assertTrue(bffFullNotificationV1.getNotificationStatusHistory().stream()
+                .filter(statusHistory -> statusHistory.getStatus().equals(BffNotificationStatus.DELIVERING))
+                .anyMatch(statusHistory -> statusHistory.getRelatedTimelineElements().stream()
+                        .anyMatch(relatedElementId -> relatedElementId.equals("relatedElementId"))
+                )
+        );
 
     }
 }
