f1b86308632d240c396d8f0c7bef7f0b4ea13f7f
alessandrogelmi <a.gelmi@codermine.com>
2026-03-02 17:06:36 +0100

    feat(PN-18580): map notification cost service client

---
+++ src/main/java/it/pagopa/pn/bff/pnclient/notificationcostservice/PnNotificationCostServiceClientImpl.java
@@ -0,0 +1,20 @@
+package it.pagopa.pn.bff.pnclient.notificationcostservice;
+
+import it.pagopa.pn.bff.generated.openapi.msclient.notification_cost_service.api.NotificationCostRecipientApi;
+import it.pagopa.pn.bff.generated.openapi.msclient.notification_cost_service.model.NotificationCostRecipientResponse;
+import lombok.CustomLog;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+@CustomLog
+@RequiredArgsConstructor
+public class PnNotificationCostServiceClientImpl {
+
+    private final NotificationCostRecipientApi notificationCostRecipientApi;
+
+    public Mono<NotificationCostRecipientResponse> getNotificationCostRecipient(String iun, Integer recIndex) {
+        return notificationCostRecipientApi.notificationCostRecipient(iun, recIndex);
+    }
+}
