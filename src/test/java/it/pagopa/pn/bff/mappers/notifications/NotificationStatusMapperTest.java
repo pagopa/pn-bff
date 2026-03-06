a2e8d67c8d55785bda90a70ff47dd599aa359ffb
Francesco Bianchi <f.bianchi@codermine.com>
2026-03-06 15:40:39 +0100

    remove empty line

--- src/test/java/it/pagopa/pn/bff/mappers/notifications/NotificationStatusMapperTest.java
+++ src/test/java/it/pagopa/pn/bff/mappers/notifications/NotificationStatusMapperTest.java
@@ -23,6 +23,4 @@ public class NotificationStatusMapperTest {
         assertNotNull(actualConvertDeliveryRecipientNotificationStatusResult);
         assertEquals(actualConvertDeliveryRecipientNotificationStatusResult.getValue(), notificationStatus.getValue());
     }
-
-
 }
